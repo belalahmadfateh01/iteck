eca700b5350fc44b933d53241680a22cf72025c6
Noorzaie9 <abdulbasirnoorzaie9@gmail.com>
2026-06-17 23:03:38 +0430

    all new routes used by app.js file

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 import express from 'express';
 
 import userRoutes from './routes/user.route.js'
+import { globalRoute as commentRoutes } from './routes/comment.route.js';
+import serviceRoutes from './routes/service.route.js';
+import projectRoutes from './routes/project.route.js';
+import teamRoute from './routes/team.route.js';
+
 
 const app = express();
 
@@ -8,6 +13,9 @@ app.use(express.json());
 
 
 app.use('/api/users', userRoutes);
-
+app.use('/api/comments', commentRoutes);
+app.use('/api/services', serviceRoutes);
+app.use('/api/projects', projectRoutes);
+app.use('/api/teams', teamRoute);
 
 export default app;
