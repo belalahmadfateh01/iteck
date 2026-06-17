ac6e9f82041b05e92e5e42a2176fdfcc4db25764
Noorzaie9 <abdulbasirnoorzaie9@gmail.com>
2026-06-17 11:49:49 +0430

    services for service project

--- services/service.service.js
+++ services/service.service.js
@@ -2,27 +2,36 @@ import pool from "../config/db.config.js";
 
 const addService = async (data) => {
   try {
-    const { name, descrption, icon } = data;
-    const query = `INSERT INTO service VALUES (DEFAULT, ?, ?, ?, DEFAULT)`;
-    const [result] = await pool.execute(query, [name, descrption, icon]);
+    if(!data) throw new Error("No data object provided");
+
+    const { name, description } = data;
+    if(!name || !description) throw new Error("name and description not provided");
+    
+    const query = `
+    INSERT INTO service(
+    name,
+    description)
+    VALUES (?, ?)`;
+
+    const [result] = await pool.execute(query, [name, description]);
     return result.insertId;
   } catch (err) {
     console.log(err);
-    return null;
+    throw new Error(err.message || 'Error in creating service');
   }
 };
 
 const getAllServices = async () => {
   const query = `SELECT * FROM service`;
   const [result] = await pool.execute(query, []);
-  return resalt;
+  return result;
 };
 
 const getService = async (id) => {
   if (!id) return null;
   const query = `SELECT * FROM service WHERE id = ?`;
   const [result] = await pool.execute(query, [id]);
-  return result;
+  return result[0] ?? null;
 };
 
 export { addService, getAllServices, getService };
