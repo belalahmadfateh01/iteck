753c1219cfe476c59496b89353ef91e4ee10ed65
Noorzaie9 <abdulbasirnoorzaie9@gmail.com>
2026-06-17 23:01:53 +0430

    adding member to team functionality add

--- services/team.service.js
+++ services/team.service.js
@@ -2,13 +2,21 @@ import pool from "../config/db.config.js";
 
 const addTeam = async (data) => {
   try {
+    if(!data) throw new Error("No data object provided");
+    
     const { name, description } = data;
-    const query = `INSERT INTO team VALUES (DEFAULT, ?, ?)`;
+    if(!name || !description) throw new Error("name or description not provided");
+    
+    const query = `
+    INSERT INTO team(
+    name,
+    description)
+    VALUES (?, ?)`;
     const [result] = await pool.execute(query, [name, description]);
     return result.insertId;
   } catch (err) {
     console.log(err);
-    return null;
+    throw new Error(err);
   }
 };
 
@@ -19,16 +27,35 @@ const getAllTeam = async () => {
 };
 
 const getTeam = async (id) => {
-  if (!id) return null;
+  if (!id) throw new Error("No id provided");
+
   const query = `SELECT * FROM team WHERE id = ?`;
-  const result = await pool.execute(query, [id]);
+  const [result] = await pool.execute(query, [id]);
+  console.log(result);
   return result;
 };
 
-const teamMember = async (id) => {
-  if (!id) return null;
-  const query = `SELECT * FROM user WHERE team_id = ?`;
+const getTeamMembers = async (id) => {
+  if (!id) throw new Error("No id provided");
+
+  const query = `SELECT * FROM users WHERE team_id = ?`;
   const [result] = await pool.execute(query, [id]);
   return result;
 };
-export { addTeam, getAllTeam, getTeam, teamMember };
+
+const addTeamMember = async (userId, teamId) => {
+  try {
+    const query = `
+    UPDATE users SET team_id = ? WHERE id = ?
+    `;
+
+    const [result] = await pool.execute(query, [teamId, userId]);
+    return result;
+
+  } catch (err) {
+    console.log(err);
+    throw new Error(err.message || 'Error in adding member to team');
+  }
+}
+
+export { addTeam, getAllTeam, getTeam, getTeamMembers, addTeamMember };
