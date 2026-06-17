3454f0e265761ab51f3ba07d844f645879a9b150
Noorzaie9 <abdulbasirnoorzaie9@gmail.com>
2026-06-17 11:42:52 +0430

    module refactore in comment service

--- services/comment.service.js
+++ services/comment.service.js
@@ -2,25 +2,39 @@ import pool from "../config/db.config.js";
 
 const addComment = async (data) => {
   try {
-    const { message, rating, date } = data;
-    const query = `INSERT INTO comment VALUES (DEFAULT, ?, ?, DEFAULT, DEFAULT, DEFAULT)`;
-    const [result] = await pool.execute(query, [message, rating, date]);
+    if(!data) throw new Error("No data object provided");
+    
+    const { message, rating, user_id, service_id } = data;
+
+    if(!message || !rating || !user_id || !service_id) throw new Error("message, rating, user id and service id are required");
+
+    const query = `
+    INSERT INTO comment(
+    message,
+    rating,
+    user_id,
+    service_id)
+    VALUES (?, ?, ?, ?)`;
+
+    const [result] = await pool.execute(query, [message, rating, user_id, service_id]);
     return result.insertId;
   } catch (err) {
     console.log(err);
-    return null;
+    throw new Error(err.message);
   }
 };
 
-const getAllComment = async () => {
-  const query = "SELECT * FROM comment";
-  const [result] = await pool.execute(query, []);
+const getAllComment = async (userId) => {
+  if(!userId) throw new Error("user id not provided");
+  const query = "SELECT * FROM comment WHERE user_id = ?";
+  const [result] = await pool.execute(query, [userId]);
   return result;
 };
 
-const getComment = async (id) => {
-  const query = "SELECT * FROM comment WHERE id = ?";
-  const [result] = await pool.execute(query, [id]);
+const getComment = async (userId, commentId) => {
+  if(!userId) throw new Error("user id not provided");
+  const query = "SELECT * FROM comment WHERE id = ? AND user_id = ?";
+  const [result] = await pool.execute(query, [commentId, userId]);
   return result;
 };
 
